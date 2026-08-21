e4c063bedbde6efcad7df6c3ad2a9bae89066759
drajj-0011 <debrajdubey0011@gmail.com>
2026-08-21 22:20:07 +0530

    Add files via upload

---
+++ monotonicArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool isMonotonic(vector<int>& nums){
+    bool decreasing=true;
+    bool increasing=true;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]>nums[i-1]){
+            decreasing=false;
+        }
+        if(nums[i]<nums[i-1]){
+            increasing=false;
+        }
+        return increasing||decreasing;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    if(isMonotonic(nums)){
+        cout<<"true";
+    }else
+    cout<<"false";
+    return 0;
+}
